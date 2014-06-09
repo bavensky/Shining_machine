f97f5ae6814f82f2fea459e38db877c0260dfe2c
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2014-06-09 19:19:13 +0700

    bavensky

--- Shining_machine/Shining_machine.ino
+++ Shining_machine/Shining_machine.ino
@@ -36,10 +36,18 @@
    #define LED1  6
    #define LED2  3
    
+   #define DEBOUNCE 200
    #define DELAY 2000
    
    LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
    
+   String message[] = { "Shining Machine ",
+                        "   Start Menu   ",
+                        "     Menu1      ",
+                        "     Menu2      ",
+                        "     Menu3      ",
+                        "   Exit Menu    "};
+                       
    int button1=0, button2=0; 
    int a=0, b=0, c=0, d=0;
    
@@ -52,39 +60,91 @@
      lcd.begin(16, 2);
      
      lcd.home();
-     lcd.print("Shining Machine");
-     
+     lcd.print(message[0]);     
    }
    
    void loop()
    {
      lcd.setCursor(0, 1);
-     lcd.print("   Start Menu   ");
-     delay(DELAY);
-     a=1;
+     lcd.print(message[1]);
+     
+     button1 = digitalRead(BUTTON1);
+     if(button1==0)
+     {
+       delay(DEBOUNCE);
+       a=1;
+     }
+     
      while(a==1)
      {
        lcd.setCursor(0, 1);
-       lcd.print("     Menu1      ");
+       lcd.print(message[2]);
        
        button1 = digitalRead(BUTTON1);
        button2 = digitalRead(BUTTON2);
        if(button1 == 0)
        {
+         delay(DEBOUNCE);
+         a=2;
+       }
+       if(button2 == 0)
+       {
+         delay(DEBOUNCE);
+         for(int lo=0; lo<15; lo++)
+         {
+           digitalWrite(LED1, HIGH);
+           delay(1000);  //  ติด 5 วินาที
+           digitalWrite(LED1, LOW);
+           delay(200);
+         }
+       } 
+     }
+     
+     while(a==2)
+     {
+       lcd.setCursor(0,1);
+       lcd.print(message[3]);
+       
+       button1 = digitalRead(BUTTON1);
+       button2 = digitalRead(BUTTON2);
+       if(button1 == 0)
+       {
+         delay(DEBOUNCE);
+         analogWrite(LED1, LOW);
+         a=3;
+       }
+       if(button2 == 0)
+       {
+         delay(DEBOUNCE);
+         for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5)
+         { 
+           analogWrite(LED1, fadeValue);        
+           delay(300);  
+         }  
+       }
+     }
+     
+     while(a==3)
+     {
+       lcd.setCursor(0,1);
+       lcd.print(message[4]);
+       button1 = digitalRead(BUTTON1);
+       button2 = digitalRead(BUTTON2);
+       if(button1 == 0)
+       {
+         delay(DEBOUNCE);
          lcd.setCursor(0, 1);
-         lcd.print("   Exit Menu   ");
-         delay(500);
+         lcd.print(message[5]);
+         delay(1000);
          a=0;
        }
        if(button2 == 0)
        {
-         for(int lo=0; lo<15; lo++)
-         {
-           digitalWrite(LED1, HIGH);
-           delay(5000);
-           digitalWrite(LED1, LOW);
-           delay(2000);
-         }
-       } 
-     }
+         digitalWrite(LED2, HIGH);
+       }
+       if(button2 == 1)
+       {
+         digitalWrite(LED2, LOW);
+       }
+     } 
    }
